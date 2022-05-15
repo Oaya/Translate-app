c04e844d96f8e1ba2caa07324392888485b2f751
Aya Okizaki <74119671+Oaya@users.noreply.github.com>
2022-05-14 18:40:37 -0700

    create getApiResonses function

--- src/Providers/ApiContext.tsx
+++ src/Providers/ApiContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 import { Configuration, OpenAIApi } from "openai";
 
 interface ApiProviderProps {
@@ -11,6 +11,7 @@ interface ProviderData {
 
 export const ApiContext = createContext<ProviderData | null>(null);
 
+
 //set congiguration for openAI//
 const config = new Configuration({
   apiKey: process.env.REACT_APP_API_KEY,
@@ -19,9 +20,40 @@ const config = new Configuration({
 const openai = new OpenAIApi(config);
 
 export default function ApiProvider({ children }: ApiProviderProps) {
+  const [responses, setResponses] = useState([]);
 
+  const getApiResponse = (query, languages) => {
+    const langString = languages?.map(({ value }) => value).join(' and ')
+    console.log(langString)
 
-  const providerData = {};
+    let prompt = `Translate this into ${langString}: \n${query}\n`;
+    console.log(prompt);
+
+    if (query && languages) {
+      openai.createCompletion('text-davinci-002', {
+        prompt: prompt,
+        temperature: 0.3,
+        max_tokens: 100,
+        top_p: 1.0,
+        frequency_penalty: 0.0,
+        presence_penalty: 0.0,
+      }).then(res => {
+        const response = res.data.choices[0].text;
+        const obj = { prompt: query, response: response }
+        setResponses((prev) => {
+          return [...prev, obj]
+        })
+        console.log(responses)
+      })
+    } else {
+      throw Error("Error, something went wrong")
+    }
+  };
+
+  const providerData = {
+    responses,
+    get
+  };
 
 
 
